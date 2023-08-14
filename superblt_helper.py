7f77fc6732c03e2766c54fc3ca4ede6d3d378d1f
stella <stel@nothanks.co>
2023-08-14 16:59:26 +0800

    updated superblt_helper.py to add keybinds

--- superblt_helper.py
+++ superblt_helper.py
@@ -1,27 +1,16 @@
 # superblt mod.txt helper
+# author: stel (discord: @stel, https://github.com/dumbsaltyho)
 import json
 
-mod_building_hooks: bool = True
-mod_name: str = input("mod name: ")
-mod_description: str = input("mod description: ")
-mod_author: str = input("mod author: ")
-mod_contact: str = input("mod contact: ")
-mod_image: str = input("mod image: ")
+mod_is_building_hooks: bool = True
+mod_name: str = input("mod name: ") or "test"
+mod_description: str = input("mod description: ") or "add later"
+mod_author: str = input("mod author: ") or "stel"
+mod_contact: str = input("mod contact: ") or "discord: @stel"
+mod_image: str = input("mod image: ") or "stel.png"
 mod_color: str = input("mod color (default: 105 65 143): ") or "105 65 143"
-mod_version: str = input("mod version (default: 1) :") or "1"
+mod_version: str = input("mod version (default: 1): ") or "1"
 mod_priority: str = input("mod priority (default: 10): ") or "10"
-mod_hook_ids: list = []
-mod_script_paths: list = []
-
-while mod_building_hooks:
-	add_mod_hook_id: str = input("hook id, when you're finished adding hooks just press enter:\n")
-	if (add_mod_hook_id == "") or (add_mod_hook_id == None):
-		break
-	else:
-		add_mod_script_path: str = input("lua script path:\n")
-
-	mod_hook_ids.append(add_mod_hook_id)
-	mod_script_paths.append(add_mod_script_path)
 
 mod_txt_json: dict = {
 	"name" : f"{mod_name}",
@@ -32,17 +21,49 @@ mod_txt_json: dict = {
 	"color" : f"{mod_color}",  
 	"version" : f"{mod_version}",
 	"priority" : f"{mod_priority}",
-	"blt_version" : "2",
-	"hooks" : [
-	]
+	"blt_version" : "2"
 }
+if (input("do you need hooks? (y/n): ") == "y"):
+	mod_txt_json["hooks"] = []
+	while mod_is_building_hooks:
+		add_mod_hook_id: str = input("hook id, when you're finished adding hooks just press enter:\n")
+		if (add_mod_hook_id == "") or (add_mod_hook_id == None):
+			break
+		else:
+			add_mod_script_path: str = input("lua script path:\n")
 
-for idx, item in enumerate(mod_hook_ids):
-	add_hook_id_and_script_path: dict = {
-		"hook_id" : f"{mod_hook_ids[idx]}",
-		"script_path" : f"{mod_script_paths[idx]}"
-	}
-	mod_txt_json["hooks"].append(add_hook_id_and_script_path)
+		add_mod_hook_info: dict = {
+			"hook_id" : f"{add_mod_hook_id}",
+			"script_path" : f"{add_mod_script_path}"
+		}
+		mod_txt_json["hooks"].append(add_mod_hook_info)
+else: pass
+
+if (input("do you need keybinds? (y/n): ") == "y"):
+	mod_txt_json["keybinds"] = []
+	while mod_is_building_hooks:
+		add_mod_keybind_id: str = input("keybind id, when you're finished adding keybinds just press enter:\n")
+		if (add_mod_keybind_id == "") or (add_mod_keybind_id == None):
+			break
+		else:
+			add_mod_keybind_name: str = input("keybind name (can be the same as the keybind id): ")
+			add_mod_keybind_desc: str = input("keybind desription: ")
+			add_mod_keybind_script_path: str = input("keybind script path: ")
+			add_mod_keybind_run_in_menu: str = input("run in menu (true / false, default: true)?: ") or "true"
+			add_mod_keybind_run_in_game: str = input("run in game (true / false, default: true)?: ") or "true"
+			add_mod_keybind_localized: str = input("localized (true / false, default: false)?") or "false"
+
+		add_mod_keybind_info: dict = {
+			"keybind_id" : f"{add_mod_keybind_id}",
+			"name" : f"{add_mod_keybind_name}",
+			"description" : f"{add_mod_keybind_desc}",
+			"script_path" : f"{add_mod_keybind_script_path}",
+			"run_in_menu" : f"{add_mod_keybind_run_in_menu}",
+			"run_in_game" : f"{add_mod_keybind_run_in_game}",
+			"localized" : f"{add_mod_keybind_localized}",
+		}
+		mod_txt_json["keybinds"].append(add_mod_keybind_info)
+else: pass
 
 print("written to mod.txt:")
 print(json.dumps(mod_txt_json, indent=4))
