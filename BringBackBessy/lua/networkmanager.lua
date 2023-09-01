62bd55f65fdb8683dd1a5071bbec9822ab8c9564
stella <stella@salty.gay>
2023-09-01 09:53:19 +0800

    changed matchmaking key for BringBackBessy

---
+++ BringBackBessy/lua/networkmanager.lua
@@ -0,0 +1,2 @@
+NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY .. "_ownamusketforhomedefensejustlikethefoundingfathersintended"
+NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY = NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY .. "_ownamusketforhomedefensejustlikethefoundingfathersintended"
