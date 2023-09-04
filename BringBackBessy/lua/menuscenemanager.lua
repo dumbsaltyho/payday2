397babddfd49f8a82728c475d4997a6eb5717731
stella <stella@salty.gay>
2023-09-04 15:10:33 +0800

    test

---
+++ BringBackBessy/lua/menuscenemanager.lua
@@ -0,0 +1,27 @@
+function MenuSceneManager:_init_lobby_poses()
+	self._lobby_poses = {
+		generic = {
+			{
+				"lobby_generic_idle1"
+			},
+			{
+				"lobby_generic_idle2"
+			},
+			{
+				"lobby_generic_idle3"
+			},
+			{
+				"lobby_generic_idle4"
+			}
+		},
+		m134 = {
+			"lobby_minigun_idle1"
+		},
+		kacchainsaw = {
+			"lobby_idle_minigun_2"
+		},
+		bessy = {
+			"lobby_idle_bessy"
+		}
+	}
+end
